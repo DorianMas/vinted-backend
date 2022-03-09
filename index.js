91002e92f558004c8b7f150a6cb0a344ef17f72d
Dorian Masquelier <dorian.masquelier@gmail.com>
2022-03-09 19:09:51 +0100

    update

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const formidableMiddleware = require("express-formidable");
 const mongoose = require("mongoose");
-
+require("dotenv").config();
 //connexion à la bdd
-mongoose.connect("mongodb://localhost/vinted");
+mongoose.connect(process.env.MONGODB_URI);
 
 //création du serveur
 const app = express();
 app.use(formidableMiddleware());
+
+//protection des ressources sur le site web
+const cors = require("cors");
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.json({ message: "Hi" });
@@ -19,6 +23,6 @@ app.use(usersRoutes);
 const offersRoutes = require("./routes/offers");
 app.use(offersRoutes);
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
   console.log("Server has started");
 });
